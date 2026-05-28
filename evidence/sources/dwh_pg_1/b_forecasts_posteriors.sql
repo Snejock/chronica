d3c701c38613f1011379e01f9954057d14b8d4da
s.chernov <s7.chernov@gmail.com>
2026-05-29 02:31:24 +0300

    Added evidence's sources

---
+++ evidence/sources/dwh_pg_1/b_forecasts_posteriors.sql
@@ -0,0 +1,21 @@
+SELECT
+    b.forecast_id
+    , b.language_code
+    , b.forecast_nm
+    , b.news_id
+    , b.published_dttm
+    , b.p_confirm_prt
+    , b.p_refute_prt
+    , b.p_posterior_prt
+    , b.alpha
+    , b.beta
+    , b.effective_n
+    , b.p_lower_95_prt
+    , b.p_upper_95_prt
+    , f.story_id
+    , f.horizon_days
+    , f.forecast_txt
+FROM bds.b_forecasts_posteriors b
+JOIN dds.d_forecasts f
+    ON  f.forecast_id   = b.forecast_id
+    AND f.language_code = b.language_code
---
+++ evidence/sources/dwh_pg_1/b_stories_news.sql
@@ -0,0 +1,9 @@
+SELECT
+    published_dttm
+    , story_id
+    , story_nm
+    , feed_nm
+    , language_code
+    , news_id
+    , news_txt
+FROM bds.b_stories_news
