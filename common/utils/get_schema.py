a4ac14cb1599fd3e1c54b84c50d3305c1d10fcad
s.chernov <s7.chernov@gmail.com>
2026-08-04 11:26:23 +0300

    Add MinIO support to docker-compose and update environment variable naming

--- common/utils/get_schema.py
+++ common/utils/get_schema.py
@@ -3,7 +3,7 @@ import json
 
 
 def get_schema(schema: str) -> str:
-    schema_dir = os.environ.get("DWH_SCHEMA_DIR", "/app/dwh/migrations/rp")
+    schema_dir = os.environ.get("DWH__SCHEMA_DIR", "/app/dwh/migrations/rp")
     schema_path = os.path.join(schema_dir, f"{schema}.json")
 
     with open(schema_path, "r") as f:
