7f42c40591f61035d074f9a74dcf5d62913ed78c
s.chernov <s7.chernov@gmail.com>
2026-08-06 15:56:46 +0300

    Update `LOAD_DDS_T_NEWS_ACTORS` to standardize `photo_link` naming, handle NBSP in regex, and refine S3 path logic

--- dwh/migrations/pg/045_create_table_dds.s_actor_media.sql
+++ dwh/migrations/pg/045_create_table_dds.s_actor_media.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS dds.s_actor_media (
-    _loaded_dttm     timestamp(0) with time zone DEFAULT now(),
-    actor_id         text NOT NULL,
-    source_link      text,
-    photo_url        text,
-    author_nm        text,
-    license_nm       text,
+    _loaded_dttm        timestamp(0) with time zone DEFAULT now(),
+    actor_id            text NOT NULL,
+    source_link         text,
+    photo_link          text,
+    author_nm           text,
+    license_nm          text,
 
     CONSTRAINT s_actor_media__actor_id_pk PRIMARY KEY (actor_id),
 
